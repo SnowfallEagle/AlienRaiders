a4aed11afe8dcc47e46ed1802f2e380627b8df8b
SeregaEngine <engineserega@gmail.com>
2023-03-14 23:46:09 +0300

    GAME_MODE_WEB define, show sticky ad and fullscreen ad on enter

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,3 +1,7 @@
+#if UNITY_WEBGL && !UNITY_EDITOR
+    #define GAME_MODE_WEB
+#endif
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +10,12 @@ public class GameManager : MonoBehaviour
 {
     private void Start()
     {
-#if UNITY_WEBGL
+#if GAME_MODE_WEB
         ServiceLocator.Instance.Add<AdService, YandexAdService>();
 #endif
+
         var AdService = ServiceLocator.Instance.Get<AdService>();
+        AdService.ToggleStickyAd(true);
         AdService.ShowFullscreenAd();
     }
 }
