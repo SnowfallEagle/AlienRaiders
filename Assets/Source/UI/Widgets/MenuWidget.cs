2378f210507457fd7c029097301ddffbdd0ffe10
SnowfallEagle <engineserega@gmail.com>
2023-04-19 20:03:48 +0300

    Add FadeImage

--- Assets/Source/UI/Widgets/MenuWidget.cs
+++ Assets/Source/UI/Widgets/MenuWidget.cs
@@ -1,7 +1,26 @@
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 public class MenuWidget : UIWidget
 {
+    [SerializeField] private Image m_FadeImage;
+
+    private BehaviorComponent m_BehaviorComponent;
+
+    public override void OnShow()
+    {
+        base.OnShow();
+
+        m_BehaviorComponent = InitializeComponent<BehaviorComponent>();
+
+        Assert.IsNotNull(m_FadeImage);
+        m_FadeImage.color = Color.black;
+        m_FadeImage.raycastTarget = false;
+
+        m_BehaviorComponent.AddAction(new BHUIAction_FadeImage(m_FadeImage, false, 0.5f));
+    }
+
     public void OnPlayClicked()
     {
         GameStateMachine.Instance.SwitchState(new FightGameState());
